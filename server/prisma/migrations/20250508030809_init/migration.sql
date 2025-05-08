e4fc922d569499618db63819e351d1a92cb723c5
cofeek-codes <kormyshev11@mail.ru>
2025-05-08 08:17:44 +0500

    prisma schema + first getter

---
+++ server/prisma/migrations/20250508030809_init/migration.sql
@@ -0,0 +1,31 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Service" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "expectedService" TEXT NOT NULL,
+    "masterName" TEXT NOT NULL,
+    "adress" TEXT NOT NULL,
+    "categoryId" INTEGER NOT NULL,
+    "userId" INTEGER,
+    CONSTRAINT "Service_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "Service_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
--- server/src/main.js
+++ server/src/main.js
@@ -1,13 +1,25 @@
 const express = require('express')
 const cors = require('cors')
+const { PrismaClient } = require('../generated/prisma/client')
 
 const app = express()
+const prisma = new PrismaClient()
 
 app.use(cors())
 
 app.get('/', (req, res) => {
     res.json("hello world")
 })
+
+app.get('/services/all', (req, res) => {
+  const services = prisma.service.findMany({
+    include: {category: true, user: true}
+  })
+  
+  res.json(services)
+
+})
+
 
 app.listen(3000, () => {
     console.log('listening on 3000')
